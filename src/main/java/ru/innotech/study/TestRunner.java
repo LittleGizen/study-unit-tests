c439922aec062633ffd76cd9efde93f76587fdee
Симонов Андрей Васильевич (4037064) <asimonov@dev.vtb.ru>
2024-09-24 22:31:19 +0300

    The class has been corrected according to the comments: 1. Stream API has been removed 2. The design is simplified 3. Fixed to one pass through methods

--- src/main/java/ru/innotech/study/TestRunner.java
+++ src/main/java/ru/innotech/study/TestRunner.java
@@ -1,91 +1,82 @@
 package ru.innotech.study;
 
+import ru.innotech.study.annotation.AfterSuite;
+import ru.innotech.study.annotation.BeforeSuite;
 import ru.innotech.study.annotation.Test;
 
-import java.lang.annotation.Annotation;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.*;
+import java.lang.reflect.Modifier;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 
 public class TestRunner {
 
     /**
      * The map with rules for limiting the number of methods with specific annotations.
      */
-    private final Map<String, Integer> methodsLimitMap = Map.of(
-            "@ru.innotech.study.annotation.BeforeSuite", 1,
-            "@ru.innotech.study.annotation.AfterSuite", 1
+    private final Map<Class, Integer> methodsLimitMap = Map.of(
+            BeforeSuite.class, 1,
+            AfterSuite.class, 1
     );
-
-    private final List<Method> methodList = new LinkedList<>();
 
     public void runTests(Class c) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException, InstantiationException {
         Object instance = c.getConstructor().newInstance();
+        Method[] methods = c.getDeclaredMethods();
 
-        checkByRules(c);
+        Method beforeSuiteMethod = null;
+        Method afterSuiteMethod = null;
+        List<Method> testList = new LinkedList<>();
 
-        methodList.add(c.getMethod("beforeSuite"));
-        methodList.addAll(getSortedTests(c));
-        methodList.add(c.getMethod("afterSuite"));
-
-        for (Method method : methodList) {
-            method.invoke(instance);
+        for (Method method : methods) {
+            if (method.isAnnotationPresent(BeforeSuite.class)) {
+                checkStaticModifier(method, BeforeSuite.class);
+                if (beforeSuiteMethod == null) {
+                    beforeSuiteMethod = method;
+                } else {
+                    System.err.printf("Annotation %s limit has been exceeded. Found another method (%s) with this annotation.\n", BeforeSuite.class, method);
+                }
+            } else if (method.isAnnotationPresent(AfterSuite.class)) {
+                checkStaticModifier(method, AfterSuite.class);
+                if (afterSuiteMethod == null) {
+                    afterSuiteMethod = method;
+                } else {
+                    System.err.printf("Annotation %s limit has been exceeded. Found another method (%s) with this annotation.\n", AfterSuite.class, method);
+                }
+            } else if (method.isAnnotationPresent(Test.class)) {
+                checkPriorityRange(method);
+                testList.add(method);
+            }
         }
-    }
 
-    /**
-     * Method for checking the class for compliance with the configured rules
-     * There is a check for the limit of methods with the declared annotations and check for priority range in Test annotation
-     * @param c - a class with methods to check
-     */
-    private void checkByRules(Class c) {
-        List<Annotation> annotationList = new ArrayList<>();
-        Arrays.stream(c.getMethods()).forEach(method -> annotationList.addAll(List.of(method.getDeclaredAnnotations())));
-
-        //Check priority range in Test annotation
-        annotationList.forEach(annotation -> {
-            if(annotation instanceof Test &&
-                    (((Test) annotation).priority() < 1 || ((Test) annotation).priority()>10 )) {
-                System.err.printf("Incorrect Test priority - %s. Should be between 1 and 10.\n", ((Test) annotation).priority());
-            }
-        });
-
-        List<String> annotationsNamesList = new ArrayList<>();
-        annotationList.forEach(annotation -> {
-            String annotationName = annotation.toString();
-            annotationsNamesList.add(annotationName.substring(0, annotationName.indexOf('(')));
-        });
-
-        //Check for the limit of methods with the declared annotations
-        methodsLimitMap.forEach((annotationName, limit) -> {
-            int frequency = Collections.frequency(annotationsNamesList, annotationName);
-            if (frequency > limit) {
-                System.err.printf("Annotation %s limit (%s) has been exceeded. Found %s methods with this annotation.\n", annotationName, limit, frequency);
-            } else {
-                System.out.printf("Annotation %s limit (%s) verification passed\n", annotationName, limit);
-            }
-        });
-    }
-
-    /**
-     * Method for getting a list of methods with a Test annotation sorted by priority
-     * @param c a class with methods annotated by Test
-     * @return list of sorted methods
-     */
-    private List<Method> getSortedTests(Class c) {
-        List<Method> testsList = new LinkedList<>();
-        Arrays.stream(c.getMethods()).forEach(method -> {
-            if (method.isAnnotationPresent(Test.class)) {
-                testsList.add(method);
-            }
-        });
-
-        testsList.sort((o1, o2) -> {
+        testList.sort((Method o1, Method o2) -> {
             int o1Priority = o1.getDeclaredAnnotation(Test.class).priority();
             int o2Priority = o2.getDeclaredAnnotation(Test.class).priority();
             return (o1Priority - o2Priority);
         });
 
-        return testsList;
+        if (beforeSuiteMethod != null) {
+            beforeSuiteMethod.invoke(instance);
+        }
+        for (Method method : testList) {
+            method.invoke(instance);
+        }
+        if (afterSuiteMethod != null) {
+            afterSuiteMethod.invoke(instance);
+        }
+    }
+
+    private void checkPriorityRange(Method method) {
+        int priority = method.getDeclaredAnnotation(Test.class).priority();
+        if (priority < 1 || priority > 10) {
+            System.err.printf("Annotation %s priority out of range (1-10) in method %s.\n", Test.class, method);
+        }
+    }
+
+    private void checkStaticModifier(Method method, Class annotationClass) {
+        if (!Modifier.isStatic(method.getModifiers())) {
+            System.err.printf("Method %s marked with %s but not static.\n", method, annotationClass);
+        }
     }
 }
